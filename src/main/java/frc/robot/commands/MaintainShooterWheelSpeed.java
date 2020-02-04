b662087794a4406c146a10f17db52e29a06e249a
CMH <cmhrobotics@gmail.com>
2020-02-03 19:59:37 -0600

    shoot command group

--- src/main/java/frc/robot/commands/MaintainShooterWheelSpeed.java
+++ src/main/java/frc/robot/commands/MaintainShooterWheelSpeed.java
@@ -20,50 +20,40 @@ public class MaintainShooterWheelSpeed extends CommandBase {
    * Creates a new MaintainShooterWheelSpeed.
    */
   private ShooterSubsystem shooterSubsystem;
-  private double desiredVelocity;
+  private double desiredUpperVelocity;
+  private double desiredLowerVelocity;
+  private  double targetLowerVelocityUnitsPer100ms;
+  private double targetUpperVelocityUnitsPer100ms;
+  
 
-  public MaintainShooterWheelSpeed(ShooterSubsystem shooterSubsystem, double desiredVelocity) {
+  public MaintainShooterWheelSpeed(ShooterSubsystem shooterSubsystem, double desiredUpperVelocity, double desiredLowerVelocity) {
     this.shooterSubsystem = shooterSubsystem;
-    this.desiredVelocity = desiredVelocity;
+    this.desiredUpperVelocity = desiredUpperVelocity;
+    this.desiredLowerVelocity = desiredLowerVelocity;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    /* Factory Default all hardware to prevent unexpected behaviour */
-    getTalon().configFactoryDefault();
-
-    /* Config sensor used for Primary PID [Velocity] */
-    getTalon().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,Constants.kPIDLoopIdx, Constants.kTimeoutMs);
-    getTalon().setSensorPhase(true);
-
-		/* Config the peak and nominal outputs */
-
-		getTalon().configNominalOutputForward(0, Constants.kTimeoutMs);
-		getTalon().configNominalOutputReverse(0, Constants.kTimeoutMs);
-		getTalon().configPeakOutputForward(1, Constants.kTimeoutMs);
-		getTalon().configPeakOutputReverse(-1, Constants.kTimeoutMs);
-
-		/* Config the Velocity closed loop gains in slot0 */
-
-		getTalon().config_kF(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kF, Constants.kTimeoutMs);
-		getTalon().config_kP(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kP, Constants.kTimeoutMs);
-		getTalon().config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kI, Constants.kTimeoutMs);
-		getTalon().config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kD, Constants.kTimeoutMs);
+    targetUpperVelocityUnitsPer100ms = desiredUpperVelocity * 4096 / 600;
+    targetLowerVelocityUnitsPer100ms = desiredLowerVelocity * 4096 / 600;
+    initTalon(shooterSubsystem.getLowerShooterMotor());
+    initTalon(shooterSubsystem.getUpperShooterMotor());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double motorOutput = getTalon().getMotorOutputPercent();
-    double targetVelocityUnitsPer100ms = desiredVelocity * 4096 / 600;
-    getTalon().set(ControlMode.Velocity, targetVelocityUnitsPer100ms);
+    //double motorOutput = getTalon().getMotorOutputPercent();
+    shooterSubsystem.getUpperShooterMotor().set(ControlMode.Velocity, targetUpperVelocityUnitsPer100ms);
+    shooterSubsystem.getLowerShooterMotor().set(ControlMode.Velocity, targetLowerVelocityUnitsPer100ms);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    getTalon().set(ControlMode.Velocity, 0);
+    shooterSubsystem.getUpperShooterMotor().set(ControlMode.Velocity, 0);
+    shooterSubsystem.getLowerShooterMotor().set(ControlMode.Velocity, 0);
   }
 
   // Returns true when the command should end.
@@ -72,9 +62,31 @@ public class MaintainShooterWheelSpeed extends CommandBase {
     return false;
   }
 
-  private WPI_TalonSRX getTalon() {
-    return null;
+
+  private void initTalon(WPI_TalonSRX talon){
+    /* Factory Default all hardware to prevent unexpected behaviour */
+    talon.configFactoryDefault();
+
+    /* Config sensor used for Primary PID [Velocity] */
+    talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,Constants.kPIDLoopIdx, Constants.kTimeoutMs);
+    talon.setSensorPhase(true);
+
+		/* Config the peak and nominal outputs */
+
+		talon.configNominalOutputForward(0, Constants.kTimeoutMs);
+		talon.configNominalOutputReverse(0, Constants.kTimeoutMs);
+		talon.configPeakOutputForward(1, Constants.kTimeoutMs);
+		talon.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+		/* Config the Velocity closed loop gains in slot0 */
+
+		talon.config_kF(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kF, Constants.kTimeoutMs);
+		talon.config_kP(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kP, Constants.kTimeoutMs);
+		talon.config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kI, Constants.kTimeoutMs);
+		talon.config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocity.kD, Constants.kTimeoutMs);
   }
+
+
 
   
 }
